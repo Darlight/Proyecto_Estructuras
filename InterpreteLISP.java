0361a1f9ae353df048a379e0cd21fd709879421f
Darlight <andres.perdomo09@gmail.com>
2019-03-11 11:44:13 -0600

    Colocando funciones de aritmetica

---
+++ InterpreteLISP.java
@@ -0,0 +1,39 @@
+/*
+Universidad del Valle de Guatemala
+Seccion - 10
+Autores:
+InterpreteLISP.java
+Proposito:
+ */
+
+public class InterpreteLISP{
+    //Unico atributo de la clase
+    private int resultado;
+
+    //Override del metodo de la interfaz Calculator
+    //Recibe como parametros dos valores int y un string
+    //Los valores int son los numero entre los cuales se realizara
+    //la operacion. Y el valor string es el operador
+    public int Calculate(int num1, int num2, String op){
+        //Condicion para cada caso
+        if (op == "+"){
+            //Si es +, entonces suma
+            resultado = num1 + num2;
+        } else if (op == "-"){
+            //Si es -, entonces resta
+            resultado = num1 - num2;
+        } else if (op == "*"){
+            //Si es *, entonces multiplica
+            resultado = num1 * num2;
+        } else if (op == "/"){
+            //Si es /, entonces divide
+            resultado = num1/num2;
+        } else {
+            //En caso de que no sea ninguno
+            //Entonces se le asignara un 0
+            resultado = 0;
+        }
+        //Devuelve el resultado
+        return resultado;
+    }
+}
