82a1112bd1f688df9cf31278843ee79770a3f4a6
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2019-03-11 12:14:12 -0600

    Working on Main

--- MainLISP.java
+++ MainLISP.java
@@ -1,8 +1,27 @@
 public class MainLISP{
+import java.util.Scanner
+
+
+
 
     public static void main(String[] args){
+    MainLisp tipo_1, tipo_2 = new MainLISP();
+    Scanner s = new Scanner(System.in);
+
+    System.out.println("Que tipo de archivo esta tratando de leer? \n 1. ");
+    int choice = s.nextInt();
 
         
+
+    }
+
+
+    //
+    public void tipo_1(){
+
+    }
+
+    public void tipo_2(){
 
     }
 }
