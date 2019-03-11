04ccb842e483014713e708fd021d004737e48b24
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2019-03-11 11:42:10 -0600

    Nichts gemacht

---
+++ TraductorLISP.java
@@ -0,0 +1,10 @@
+/*
+Universidad del Valle de Guatemala
+Estructura de datos - Seccion 10
+traductorLisp.java
+Proposito: Traduce la sintaxis de Lisp a String
+ */
+class TraductorLisp{
+    String instrucciones = "";
+    ArrayList<E> directorio;
+}
