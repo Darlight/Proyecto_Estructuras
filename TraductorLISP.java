dd4ed6b1e6f8f651916b0423821c241b20f72eb6
Darlight <andres.perdomo09@gmail.com>
2019-03-15 17:07:17 -0600

    Debugging TraductorLISP.java

--- TraductorLISP.java
+++ TraductorLISP.java
@@ -5,7 +5,8 @@ traductorLisp.java
 Proposito: Traduce la sintaxis de Lisp a String, ayuda al Parser
  */
 import java.util.*;
-
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 class TraductorLisp{
     //Crear getters y setters
     String inputLine;
@@ -53,31 +54,31 @@ class TraductorLisp{
     }
 
     // Utiliza el token actual como patron
-    public SExpression getIdentifier() throws customException{
+    public SExpression getIdentifier() throws exceptionError{
         String identifier = tokens[currentToken];
         //Compara la longitud de un string
         if(identifier.length() > 20){
             symbolLimite = true;
-            throw new customException(getError(), "Parsing");
+            throw new exceptionError(getError(), "Parsing");
         }
         currentToken++;
-        return SExpression.getFromTable(identifier);
+        return SExpression.getTable(identifier);
     }
 
     // Revisa si es un entero
     public boolean isInteger(){
-        // Compara patarones de enteros utilizando signos de suma o resta
-        Matcher matchInteger= Pattern.compile("[+-]?\\d+").matcher(tokens[currentToken]);
+        // Compara patrones de enteros utilizando signos de suma o resta
+        Matcher matchInteger = Pattern.compile("[+-]?\\d+").matcher(tokens[currentToken]);
         return matchInteger.matches();
     }
 
     // Consigue el entero del token actual
-    public SExpression getInteger() throws customException {
+    public SExpression getInteger() throws exceptionError {
         String num = tokens[currentToken];
         // Check for length limit
         if(num.length() > 20){
             intLimite = true;
-            throw new customException(getError(), "Parsing");
+            throw new exceptionError(getError(), "Parsing");
         }
         currentToken++;
         return new SExpression(Integer.parseInt(num));
@@ -90,17 +91,19 @@ class TraductorLisp{
 
     // Revisa si es un nodo izquierdo del arbol
     public boolean isLeftBrace(){
+
         return tokens[currentToken].equals("(");
     }
 
     // Revisa si es un nodo derecho del arbol
     public boolean isRightBrace(){
+
         return tokens[currentToken].equals(")");
     }
 
     // Revisa si hay mas tokens que deberan ser enviado por el Parser
     public boolean hasMoreTokens(){
-        return currentToken < numberOfTokens;
+        return currentToken < num_tokens;
     }
 
 
@@ -133,5 +136,51 @@ class TraductorLisp{
         }
 
         return found;
+    }
+
+    public String getError(){
+        String error;
+        if(!hasMoreTokens()){
+            if(countBrace > 0)
+                error = "Falta el parentesis derecho.**";
+            else
+                error = "Expresion de input terminado inesperadamente.**";
+        }
+        else if(isDot()){
+            error = "Punto inesperado.**";
+        }
+        else if(isLeftBrace()){
+            error = "Parentesis izquierdo inesperado.**";
+        }
+        else if(isRightBrace()){
+            error = "Parentesis derecho extra inesperado.**";
+        }
+        else if(isLeftBrace()){
+            error = "Parentesis izquierdo inesperado.**";
+        }
+        else if(symbolLimite){
+            error = "Límite de longitud del símbolo excedido para el símbolo '" + tokens[currentToken] + "' **";
+            symbolLimite = false;
+        }
+        else if(intLimite){
+            error = "Se ha excedido el límite de longitud de entero para entero '" + tokens[currentToken] + "' **";
+            intLimite = false;
+        }
+        else if(checkIfInvalid() != null){
+            error = "Caracter invalido '" + checkIfInvalid() + "' encontrado al analizar la expresión de entrada de análisis**";
+        }
+        else if(checkIfLowerCase() != null){
+            error = "Lower Case character '" + checkIfLowerCase() + "' encontrado al analizar la expresión de entrada de análisis **";
+        }
+        else if(tokens[currentToken].equals("+")){
+            error = "Carácter no válido '+' encontrado al analizar. Si desea ingresar un número entero, elimine los espacios.";
+        }
+        else if(tokens[currentToken].equals("-")){
+            error = "Carácter no válido '-' encontrado al analizar. Si desea ingresar un número entero, elimine los espacios.";
+        }
+        else
+            error = "Se encontró un token inesperado'" + tokens[currentToken] + "' al analizar la entrada. Compruebe si ha introducido una expresión de entrada válida. ** ";
+
+        return error;
     }
 }
