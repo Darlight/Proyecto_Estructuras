5dd148f5d228dd70ce91519b05e93e96a9d9b6cb
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2019-03-15 10:14:19 -0600

    Finished Evaluador

--- Evaluador.java
+++ Evaluador.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 public class Evaluador{
     AList aL;
 
-    // Contructor to invoke new AList for each
+    // Contructor 
     public Evaluator(){
         aL = new AList();
     }
 
     public SExpression eval(SExpression exp) throws customException{
-        // This function implements eval function.
+      
 
-        // If S-Expression is an atom,
-        // it gets the value from either symbol table or AList
+        // Si la expression es un atom
+        // Agarra los valores con signo
         if(exp.isAtom()){
             if(exp.isT() || exp.isNil() || exp.isInteger()){
                 return exp;
@@ -22,14 +22,14 @@ public class Evaluador{
                 return aL.getVal(exp);
         }
 
-        // If not atom, take car and check what kind of function it is.
+        // Chequea que tipo de funcion es
         else if(exp.car().isSymbol()){
             SExpression c1 = exp.car();
             SExpression c2 = exp.cdr();
             String nombre_exp = c1.name;
 
             if(nombre_exp.equals("QUOTE")){
-                // If valid arguments are given, just return c2 of the S-Expression
+                // Retorna pos si tiene valores correctos
                 String error_en_consola = checkIfValidArgs("QUOTE", c2);
                 if(error_en_consola != null)
                     throw new customException(error_en_consola, "Evaluacion Erronea");
@@ -37,17 +37,17 @@ public class Evaluador{
                     return c2.car();
             }
             else if(nombre_exp.equals("COND")){
-                // For COND, call eval_con
+                // Llama a COND
                 if(c2.isNil())
                     throw new customException("Argumentos no encontrados. **", "Evaluacion");
                 return eval_con(c2);
             }
             else if(nombre_exp.equals("DEFUN")){
-                // If Defun, check if it defined properly with function nombre_exp, parametros_funcion and body
+                // Chequea si algo es una funcion o no
 
-                String error = checkIfValidDefun(c2);
-                if(error != null)
-                    throw new customException(error, "Evaluation");
+                String error_en_consola = checkIfValidDefun(c2);
+                if(error_en_consola != null)
+                    throw new customException(error_en_consola, "Evaluacion");
 
                 // If all arguments are valid, create a Defun object with nombre_exp, params and body
                 SExpression nombre_de_funcion = c2.car().car();
@@ -59,8 +59,6 @@ public class Evaluador{
 
                 return SExpression.getTable(nombre_de_funcion.name);
             }
-            // else if(c2.isSymbol())
-            // 	throw new customException("Could not evaluate" + exp.displayTree() + " .**", "Evaluation");
 
             else{
                 SExpression resultado_1;
@@ -80,15 +78,15 @@ public class Evaluador{
 
 
     public SExpression eval_con(SExpression be) throws customException{
-        // This function implemets eval_con function.
+        // Implementacion de eval_con
 
         if(be.isNil()){
             throw new customException("Error blooleano.** ", "Evaluacion");
         }
 
-        String error_codigo = checkIfValidArgs("EVAL_CON", be.car());
-        if(error_codigo != null)
-            throw new customException(error_codigo, "Evaluation");
+        String error_en_consola = checkIfValidArgs("EVAL_CON", be.car());
+        if(error_en_consola != null)
+            throw new customException(error_en_consola, "Evaluacion");
 
         SExpression booleano = be.car().car();
         SExpression exp = be.car().cdr().car();
@@ -117,9 +115,9 @@ public class Evaluador{
 
         // Check if funcion_primaria has valid arguments
         String nombre_funcion = funcion_primaria.name;
-        String error_codigo = checkIfValidArgs(nombre_funcion, parametros_arg);
-        if(error_codigo != null)
-            throw new customException(error_codigo, "Evaluation");
+        String error_en_consola = checkIfValidArgs(nombre_funcion, parametros_arg);
+        if(error_en_consola != null)
+            throw new customException(error_en_consola, "Evaluacion");
 
         // Check if funcion_primaria is in a list of built-in functions
         if(nombre_funcion.equals("CAR"))
@@ -178,62 +176,62 @@ public class Evaluador{
 
         // If not built-in, check if in the DList
         else{
-            Defun defun = DList.getFunction(nombre_funcion);
+            Defun defun_arg = DList.getFunction(nombre_funcion);
 
             // If not in the DList, undefined funcion_primaria
-            if(defun == null)
-                throw new customException("Undefined funcion_primaria " + nombre_funcion + " . **", "Evaluation");
+            if(defun_arg == null)
+                throw new customException("Undefined funcion_primaria " + nombre_funcion + " . **", "Evaluacion");
 
             // Add arguments to AList
-            aL.addPairs(nombre_funcion, defun.parameters, aL.getArgumentsAsList(parametros_arg));
+            aL.addPairs(nombre_funcion, defun_arg.parametros_1, aL.getArgumentsAsList(parametros_arg));
             // Execute the funcion_primaria
-            SExpression exp = eval(defun.funBody);
+            SExpression exp = eval(defun_arg.cuerpo_de_funcion);
             // Remove the pairs from AList
-            aL.destroyPairs(defun.parameters);
+            aL.destroyPairs(defun_arg.parametros_1);
             return exp;
         }
     }
 
     // Helper function to check for valid arguments
 
-    public String checkIfValidDefun(SExpression defun){
-        String error_codigo = null;
+    public String checkIfValidDefun(SExpression defun_arg){
+        String error_en_consola = null;
 
         // Function name
-        if(defun.isNil())
-            error_codigo = "Function name and body cannot be null. **";
-        else if(defun.car().isNil())
-            error_codigo = "Empty function name and parameters. **";
-        else if(!defun.car().car().isSymbol())
-            error_codigo = "Function name should be a symbolic atom. **";
+        if(defun_arg.isNil())
+            error_en_consola = "Function name and body cannot be null. **";
+        else if(defun_arg.car().isNil())
+            error_en_consola = "Empty function name and parametros_1. **";
+        else if(!defun_arg.car().car().isSymbol())
+            error_en_consola = "Function name should be a symbolic atom. **";
 
-            // Function parameters
-        else if(defun.car().cdr().isNil() || defun.car().cdr().car().isNil())
-            error_codigo = "Parameters of the function cannot be empty. **";
+            // Function parametros_1
+        else if(defun_arg.car().cdr().isNil() || defun_arg.car().cdr().car().isNil())
+            error_en_consola = "Parameters of the function cannot be empty. **";
 
-        else if(!defun.car().cdr().cdr().isNil())
-            error_codigo = "Parameters should be defined as a separate list from function name. **";
+        else if(!defun_arg.car().cdr().cdr().isNil())
+            error_en_consola = "Parameters should be defined as a separate list from function name. **";
 
-        else if(defun.car().cdr().isAtom())
-            error_codigo = "Parameters to the function cannot be an atom. **";
+        else if(defun_arg.car().cdr().isAtom())
+            error_en_consola = "Parameters to the function cannot be an atom. **";
 
             // Function body
-        else if(defun.cdr().isNil() || defun.cdr().car().isNil())
-            error_codigo = "Function body cannot be empty. **";
-        else if(defun.cdr().isAtom())
-            error_codigo = "Function body cannot be an atom. **";
+        else if(defun_arg.cdr().isNil() || defun_arg.cdr().car().isNil())
+            error_en_consola = "Function body cannot be empty. **";
+        else if(defun_arg.cdr().isAtom())
+            error_en_consola = "Function body cannot be an atom. **";
 
             // If the part after function body is not NIL, it cannot be evaluated.
-        else if(!defun.cdr().cdr().isNil())
-            error_codigo = "Unexpected expression " + defun.cdr().cdr().displayTree() + " found after function body. **";
+        else if(!defun_arg.cdr().cdr().isNil())
+            error_en_consola = "Unexpected expression " + defun_arg.cdr().cdr().displayTree() + " found after function body. **";
 
-        return error_codigo;
+        return error_en_consola;
     }
 
     public String checkIfValidArgs(String funcion_necesaria, SExpression argumentos){
         // Function to check if given functions had valid arguments
 
-        String error_codigo = null;
+        String error_en_consola = null;
         int argumentos_prim = countArgs(argumentos);
         String nombre_funcion = funcion_necesaria;
 
@@ -245,75 +243,75 @@ public class Evaluador{
         switch(nombre_funcion){
             case "QUOTE":
                 if(argumentos_prim != 1)
-                    error_codigo = "QUOTE expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
-                return error_codigo;
+                    error_en_consola = "QUOTE expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
+                return error_en_consola;
 
             case "EVCON":
                 if(argumentos_prim != 2)
-                    error_codigo = "EVCON expects two arguments. " + Integer.toString(argumentos_prim) + " given. **";
-                return error_codigo;
+                    error_en_consola = "EVCON expects two arguments. " + Integer.toString(argumentos_prim) + " given. **";
+                return error_en_consola;
 
             case "CAR":
                 if(argumentos_prim != 1)
-                    error_codigo = "CAR expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
+                    error_en_consola = "CAR expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
                 else if(argumentos.car().isAtom())
-                    error_codigo =  "Invalid argument to CAR. " + argumentos.car().getName() + " is an atom. **";
-                return error_codigo;
+                    error_en_consola =  "Invalid argument to CAR. " + argumentos.car().getName() + " is an atom. **";
+                return error_en_consola;
 
             case "CDR":
                 if(argumentos_prim != 1)
-                    error_codigo = "CDR expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
+                    error_en_consola = "CDR expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
                 else if(argumentos.car().isAtom())
-                    error_codigo =  "Invalid argument to CDR. " + argumentos.car().getName() + " is an atom. **";
-                return error_codigo;
+                    error_en_consola =  "Invalid argument to CDR. " + argumentos.car().getName() + " is an atom. **";
+                return error_en_consola;
 
             case "CONS":
                 if(argumentos_prim != 2)
-                    error_codigo = "CONS expects two arguments. " + Integer.toString(argumentos_prim) + " given. **";
-                return error_codigo;
+                    error_en_consola = "CONS expects two arguments. " + Integer.toString(argumentos_prim) + " given. **";
+                return error_en_consola;
 
             case "ATOM":
                 if(argumentos_prim != 1)
-                    error_codigo = "ATOM expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
-                return error_codigo;
+                    error_en_consola = "ATOM expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
+                return error_en_consola;
 
             case "EQ":
                 if(argumentos_prim != 2)
-                    error_codigo = "EQ expects two arguments. " + Integer.toString(argumentos_prim) + " given. **";
-                return error_codigo;
+                    error_en_consola = "EQ expects two arguments. " + Integer.toString(argumentos_prim) + " given. **";
+                return error_en_consola;
 
             case "NULL":
                 if(argumentos_prim != 1)
-                    error_codigo = "NULL expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
-                return error_codigo;
+                    error_en_consola = "NULL expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
+                return error_en_consola;
 
             case "INT":
                 if(argumentos_prim != 1)
-                    error_codigo = "INT expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
-                return error_codigo;
+                    error_en_consola = "INT expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
+                return error_en_consola;
 
             case "BINARY":
                 if(argumentos_prim != 2)
-                    error_codigo = funcion_necesaria + " expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
+                    error_en_consola = funcion_necesaria + " expects exactly one argument. " + Integer.toString(argumentos_prim) + " given. **";
                 else if(!argumentos.car().isInteger() || !argumentos.cdr().car().isInteger())
-                    error_codigo = "Arguments to " + funcion_necesaria + " must be integer atoms. **";
+                    error_en_consola = "Arguments to " + funcion_necesaria + " must be integer atoms. **";
                 else if((funcion_necesaria.equals("QUOTIENT") || funcion_necesaria.equals("REMAINDER")) && argumentos.cdr().car().val == 0)
-                    error_codigo = "Cannot perform division with divisor value as 0";
-                return error_codigo;
+                    error_en_consola = "Cannot perform division with divisor value as 0";
+                return error_en_consola;
 
             default:
-                return error_codigo;
+                return error_en_consola;
         }
     }
 
     public int countArgs(SExpression exp){
         // This function just counts number of arguments using car till NIL occurs
-        int counter = 0;
+        int contador = 0;
         while(!exp.isNil()){
             exp = exp.cdr();
-            counter += 1;
+            contador += 1;
         }
-        return counter;
+        return contador;
     }
 
 }
