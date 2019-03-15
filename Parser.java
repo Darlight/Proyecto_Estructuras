d4e6421524c338fe0e9e26ac0ba1577256637ba1
LoboAnimae <andyquan12@gmail.com>
2019-03-14 19:18:24 -0600

    Parser gemacht

--- Parser.java
+++ Parser.java
@@ -1,5 +1,65 @@
-import java.util.*;
+public class Parser {
+    public SExpression parse(String inputBuffer) throws customException {
 
-public class Parser{
+        if (inputBuffer.isEmptu()) {
+            throw new customException("No valido. ");
 
+        } else {
+            Tokenizer t = new Tokenizer(inputBuffer);
+
+            SExpression e = parseCar(tokenizer, false);
+
+            if (tokenizer.hasMoreTokens()) {
+                throw new customException("Error");
+            }
+            return e;
+        }
+    }
+
+    public SExpression parseCdr(Tokenizer t, boolean s) throws customException {
+        if (t.hasMoreTokens()) {
+            if (t.isRightBrace()) {
+                t.skip();
+
+                t.count--;
+                return SExpression.getTable("NIL");
+            } else if (t.isDot()) {
+                if (s) {
+                    t.skip();
+                    SExpresion fr = parseCar(t, false);
+                    if (t.isRightBrace() && t.hasMoreTokens()) {
+                        t.skip();
+                        t.count--;
+                        return fr;
+                    } else {
+                        System.out.println("Error");
+
+                    }
+
+                } else {
+                    System.out.println("Error");
+                }
+            } else {
+                SExpression c1 = parseCar(t, false);
+                SExpression c2 = parseCdr(t, false);
+
+            }
+            else{
+                System.out.println("Error");
+            }
+        }
+    }
+
+
+    //FINISH THIS
+    public SExpression parseCar(Tokenizer t, boolean s) throws customException {
+        if(t.isLeftBrace()){
+            tokenizer.count++;
+            t.skip();
+
+            if(t.isRightBrace()){
+                t.skip();
+            }
+        }
+    }
 }
